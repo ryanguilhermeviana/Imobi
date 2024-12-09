974290abb20f82279c2722ede037b8d979df4abf
ryanguilhermeviana <ryansantosviana2017@gmail.com>
2024-12-09 16:34:00 -0300

    view cadastrar concluída

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        if len(username.strip()) == 0 or len(email.strip()) == 0 or \
+        len(senha.strip()) == 0:
+            messages.add_message(request, 
+                                 constants.ERROR, 'Preencha todos os campos')
+            return redirect('cadastro')
+        
+        user = User.objects.filter(username=username)
+        
+        if user.exists():
+            messages.add_message(request, 
+                                 constants.ERROR, 
+                                 'Já existe um usuário com esse nome cadastrado')
+            return redirect('cadastro')
+        
+        try:
+            user = User.objects.create_user(username=username, 
+                                       email=email, 
+                                       password=senha)
+            user.save()
+            messages.add_message(request, 
+                                 constants.SUCCESS, 
+                                 'Cadastro realizado com sucesso')
+            return redirect('logar')
+        except:
+            messages.add_message(request, 
+                                 constants.ERROR, 'Erro interno do sistema')
+            return redirect('cadastro')
 
 def logar(request):
-    ...
+    return render(request, 'logar.html')
