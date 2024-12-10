3143cbe1c951fb158e75e9150ab5a9726f81b7ca
ryanguilhermeviana <ryansantosviana2017@gmail.com>
2024-12-10 09:36:14 -0300

    Modificações na view home. Template home criado.

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from plataforma.models import Imovel
 
 
 @login_required(login_url='logar')
 def home(request):
-    return render(request, 'home.html')
+    imoveis = Imovel.objects.all()
+
+    context = {
+        'imoveis': imoveis
+    }
+    
+    return render(request, 'home.html', context)
