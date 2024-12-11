fc0090e7e00311b44fb34d76a763aaa07738a8a2
ryanguilhermeviana <ryansantosviana2017@gmail.com>
2024-12-11 09:41:25 -0300

    Ajustes na view home

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from plataforma.models import Imovel
+from plataforma.models import Imovel, Cidade
 
 
 @login_required(login_url='logar')
 def home(request):
-    imoveis = Imovel.objects.all()
+    preco_minimo = request.GET.get('preco_minimo')
+    preco_maximo = request.GET.get('preco_maximo')
+    cidade = request.GET.get('cidade')
+    tipo = request.GET.getlist('tipo')
+    cidades = Cidade.objects.all()
+
+    if preco_minimo or preco_maximo or cidade or tipo:
+        
+        if not preco_minimo:
+            preco_minimo = 0
+        if not preco_maximo:
+            preco_maximo = 999999999
+        if not tipo:
+            tipo = ['A', 'C']
+        
+        
+        imoveis = Imovel.objects.filter(valor__gte=preco_minimo)\
+        .filter(valor__lte=preco_maximo)\
+        .filter(tipo_imovel__in=tipo).filter(cidade=cidade)
+    else:
+        imoveis = Imovel.objects.all()
 
     context = {
-        'imoveis': imoveis
+        'imoveis': imoveis,
+        'cidades': cidades,
     }
     
     return render(request, 'home.html', context)
